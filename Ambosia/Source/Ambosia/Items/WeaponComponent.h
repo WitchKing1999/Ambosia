0500ca85fbb5614823938c1e5bd6dfd14ecf4324
Jan-Oliver Opdenhövel <jan.opdenhoevel@gmx.de>
2015-12-31 11:25:29 +0100

    Kommentieren des Weapon Components

--- Ambosia/Source/Ambosia/Items/WeaponComponent.h
+++ Ambosia/Source/Ambosia/Items/WeaponComponent.h
@@ -6,7 +6,7 @@
 #include "WeaponComponent.generated.h"
 
 /*
- This Item hurts everything!
+Base class for weapons.
 */
 UCLASS(ClassGroup = (Items), meta = (BlueprintSpawnableComponent))
 class AMBOSIA_API UWeaponComponent : public UItemComponent
@@ -16,31 +16,67 @@ class AMBOSIA_API UWeaponComponent : public UItemComponent
 public:
 	UWeaponComponent();
 
+	/*
+	Override from ItemComponent.
+	Returns true if the time till cooled is equal/smaller than zero
+	and if the player has enough mana to use us.
+	*/
 	virtual bool Action_Implementation() override;
 
+	/*
+	Multiplies the attack points by our attack factor and returns them.
+	If we have a critical hit, it also multiplies the attack points by 2.
+	*/
 	virtual float ModifyAttackPoints_Implementation(float AttackPoints) override;
 
+	/*
+	Multiplies the magical attack points by our magical attack factor.
+	*/
 	virtual float ModifyMagicalAttackPoints_Implementation(float MagicalAttackPoints) override;
 
-	float GetAttackFactor();
-
+	/*
+	Returns our mana costs
+	*/
 	float GetManaCost();
 
+	/*
+	Returns or attack factor.
+	*/
+	float GetAttackFactor();
+
+	/*
+	Returns our magical attack factor
+	*/
 	float GetMagicalAttackFactor();
 
+	/*
+	Returns our chance for a critical hit.
+	*/
 	float GetCriticalDamageChance();
 	
 protected:
 
+	/*
+	Our attack factor
+	*/
 	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Weapon")
 	float AttackFactor;
 
+	/*
+	Our mana costs
+	*/
 	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Weapon")
 	float ManaCost;
 
+	/*
+	Our magical attack factor
+	*/
 	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Weapon")
 	float MagicalAttackFactor;
 
+	/*
+	Our chance for a critical hit.
+	*/
 	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Weapon")
 	float CriticalDamageChance;
 };
